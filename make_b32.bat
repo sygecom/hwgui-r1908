ea158b32040da11853b75060c62b0a588723f697
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2024-12-01 14:13:08 -0300

    Atualiza arquivos relacionados com BCC 32-bit/64-bit Cria as bibliotecas em lib\bc32 (32-bit) ou lib\bc64 (64-bit). Cria os objetos em obj\bc32 (32-bit) ou obj\bc64 (64-bit).

--- make_b32.bat
+++ make_b32.bat
@@ -3,8 +3,10 @@ if "%1" == "clean" goto CLEAN
 if "%1" == "CLEAN" goto CLEAN
 
 if not exist lib md lib
+if not exist lib\b32 md lib\b32
 if not exist obj md obj
 if not exist obj\b32 md obj\b32
+if not exist obj\b32\bin md obj\b32\bin
 if not exist obj\b32\mt md obj\b32\mt
 :BUILD
 
@@ -27,10 +29,12 @@ if errorlevel 1 goto BUILD_ERR
    goto EXIT
 
 :CLEAN
-   del lib\*.lib
-   del lib\*.bak
+   del lib\b32\*.lib
+   del lib\b32\*.bak
    del obj\b32\*.obj
    del obj\b32\*.c
+   del obj\b32\bin\*.exe
+   del obj\b32\bin\*.dll
    del obj\b32\mt\*.obj
    del obj\b32\mt\*.c
 
