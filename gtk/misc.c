d42cdf100d5f6efa5fb98c4873374e5b69632f32
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2025-01-25 07:39:48 -0300

    Formatação

--- gtk/misc.c
+++ gtk/misc.c
@@ -17,175 +17,179 @@
 #include "item.api"
 #include "gtk/gtk.h"
 
-void hwg_writelog( char* s )
+void hwg_writelog(char *s)
 {
 #ifdef __XHARBOUR__
-   FHANDLE handle;
+  FHANDLE handle;
 #else
-   HB_FHANDLE handle;
+  HB_FHANDLE handle;
 #endif
 
-   if( hb_fsFile( "ac.log" ) )
-      handle = hb_fsOpen( "ac.log", FO_WRITE );
-   else
-      handle = hb_fsCreate( "ac.log", 0 );
+  if (hb_fsFile("ac.log"))
+  {
+    handle = hb_fsOpen("ac.log", FO_WRITE);
+  }
+  else
+  {
+    handle = hb_fsCreate("ac.log", 0);
+  }
+  
+  hb_fsSeek(handle, 0, SEEK_END);
+  hb_fsWrite(handle, (unsigned char *)s, strlen(s));
+  hb_fsWrite(handle, (unsigned char *)"\n\r", 2);
 
-   hb_fsSeek( handle,0, SEEK_END );
-   hb_fsWrite( handle, (unsigned char *) s, strlen(s) );
-   hb_fsWrite( handle, (unsigned char *) "\n\r", 2 );
-
-   hb_fsClose( handle );
+  hb_fsClose(handle);
 }
 
-HB_FUNC( HWG_SETDLGRESULT )
+HB_FUNC(HWG_SETDLGRESULT)
 {
-   // SetWindowLong( (HWND) hb_parnl(1), DWL_MSGRESULT, hb_parni(2) );
+  // SetWindowLong( (HWND) hb_parnl(1), DWL_MSGRESULT, hb_parni(2) );
 }
 
-HB_FUNC( SETCAPTURE )
-{
-}
-
-HB_FUNC( RELEASECAPTURE )
-{
-}
-
-HB_FUNC( COPYSTRINGTOCLIPBOARD )
+HB_FUNC(SETCAPTURE)
 {
 }
 
-HB_FUNC( HWG_LOWORD )
+HB_FUNC(RELEASECAPTURE)
 {
-   hb_retni( (int) ( hb_parnl( 1 ) & 0xFFFF ) );
+}
+
+HB_FUNC(COPYSTRINGTOCLIPBOARD)
+{
+}
+
+HB_FUNC(HWG_LOWORD)
+{
+  hb_retni((int)(hb_parnl(1) & 0xFFFF));
 }
 
 HB_FUNC_TRANSLATE(LOWORD, HWG_LOWORD);
 
-HB_FUNC( HWG_HIWORD )
+HB_FUNC(HWG_HIWORD)
 {
-   hb_retni( (int) ( ( hb_parnl( 1 ) >> 16 ) & 0xFFFF ) );
+  hb_retni((int)((hb_parnl(1) >> 16) & 0xFFFF));
 }
 
 HB_FUNC_TRANSLATE(HIWORD, HWG_HIWORD);
 
-HB_FUNC( HWG_BITOR )
+HB_FUNC(HWG_BITOR)
 {
-   hb_retnl( hb_parnl(1) | hb_parnl(2) );
+  hb_retnl(hb_parnl(1) | hb_parnl(2));
 }
 
-HB_FUNC( HWG_BITAND )
+HB_FUNC(HWG_BITAND)
 {
-   hb_retnl( hb_parnl(1) & hb_parnl(2) );
+  hb_retnl(hb_parnl(1) & hb_parnl(2));
 }
 
-HB_FUNC( HWG_BITANDINVERSE )
+HB_FUNC(HWG_BITANDINVERSE)
 {
-   hb_retnl( hb_parnl(1) & (~hb_parnl(2)) );
+  hb_retnl(hb_parnl(1) & (~hb_parnl(2)));
 }
 
-HB_FUNC( SETBIT )
+HB_FUNC(SETBIT)
 {
-   if( hb_pcount() < 3 || hb_parni( 3 ) )
-      hb_retnl( hb_parnl(1) | ( 1 << (hb_parni(2)-1) ) );
-   else
-      hb_retnl( hb_parnl(1) & ~( 1 << (hb_parni(2)-1) ) );
+  if (hb_pcount() < 3 || hb_parni(3))
+  {
+    hb_retnl(hb_parnl(1) | (1 << (hb_parni(2) - 1)));
+  }
+  else
+  {
+    hb_retnl(hb_parnl(1) & ~(1 << (hb_parni(2) - 1)));
+  }  
 }
 
-HB_FUNC( CHECKBIT )
+HB_FUNC(CHECKBIT)
 {
-   hb_retl( hb_parnl(1) & ( 1 << (hb_parni(2)-1) ) );
+  hb_retl(hb_parnl(1) & (1 << (hb_parni(2) - 1)));
 }
 
-HB_FUNC( HWG_SIN )
+HB_FUNC(HWG_SIN)
 {
-   hb_retnd( sin( hb_parnd(1) ) );
+  hb_retnd(sin(hb_parnd(1)));
 }
 
-HB_FUNC( HWG_COS )
+HB_FUNC(HWG_COS)
 {
-   hb_retnd( cos( hb_parnd(1) ) );
+  hb_retnd(cos(hb_parnd(1)));
 }
 
 #ifndef __XHARBOUR__
-HB_FUNC( NUMTOHEX )
+HB_FUNC(NUMTOHEX)
 {
-   HB_ULONG ulNum;
-   int iCipher;
-   char ret[32];
-   char tmp[32];
-   int len = 0, len1 = 0;
+  HB_ULONG ulNum;
+  int iCipher;
+  char ret[32];
+  char tmp[32];
+  int len = 0, len1 = 0;
 
-   ulNum = (HB_ULONG) hb_parnl( 1 );
+  ulNum = (HB_ULONG)hb_parnl(1);
 
-   while ( ulNum > 0 )
-   {
-      iCipher = ulNum % 16;
-      if ( iCipher < 10 )
-      {
-         tmp[ len++ ] = '0' + iCipher;
-      }
-      else
-      {
-         tmp[ len++ ] = 'A' + (iCipher - 10 );
-      }
-      ulNum >>=4;
+  while (ulNum > 0)
+  {
+    iCipher = ulNum % 16;
+    if (iCipher < 10)
+    {
+      tmp[len++] = '0' + iCipher;
+    }
+    else
+    {
+      tmp[len++] = 'A' + (iCipher - 10);
+    }
+    ulNum >>= 4;
+  }
 
-   }
+  while (len > 0)
+  {
+    ret[len1++] = tmp[--len];
+  }
+  ret[len1] = '\0';
 
-   while ( len > 0 )
-   {
-      ret[len1++] = tmp[ --len ];
-   }
-   ret[len1] = '\0';
-
-   hb_retc( ret );
+  hb_retc(ret);
 }
 #endif
 
-HB_FUNC( GETDESKTOPWIDTH )
+HB_FUNC(GETDESKTOPWIDTH)
 {
-    hb_retni(gdk_screen_width());
+  hb_retni(gdk_screen_width());
 }
 
-
-HB_FUNC( GETDESKTOPHEIGHT )
+HB_FUNC(GETDESKTOPHEIGHT)
 {
-    hb_retni(gdk_screen_height());
+  hb_retni(gdk_screen_height());
 }
 
-
-HB_FUNC( HWG_HIDEWINDOW )
+HB_FUNC(HWG_HIDEWINDOW)
 {
-    gtk_widget_hide( (GtkWidget *) HB_PARHANDLE(1) );
+  gtk_widget_hide((GtkWidget *)HB_PARHANDLE(1));
 }
 
 HB_FUNC_TRANSLATE(HIDEWINDOW, HWG_HIDEWINDOW);
 
-HB_FUNC( HWG_SHOWWINDOW )
+HB_FUNC(HWG_SHOWWINDOW)
 {
-   gtk_widget_show( (GtkWidget *) HB_PARHANDLE(1) );
+  gtk_widget_show((GtkWidget *)HB_PARHANDLE(1));
 }
 
 HB_FUNC_TRANSLATE(SHOWWINDOW, HWG_SHOWWINDOW);
 
-HB_FUNC( HWG_SHOWALL )
+HB_FUNC(HWG_SHOWALL)
 {
-   gtk_widget_show_all( (GtkWidget *) HB_PARHANDLE(1) );
+  gtk_widget_show_all((GtkWidget *)HB_PARHANDLE(1));
 }
 
-HB_FUNC( SENDMESSAGE )
-{
-}
-
-HB_FUNC( GETNOTIFYCODE )
+HB_FUNC(SENDMESSAGE)
 {
 }
 
-HB_FUNC( TREENOTIFY )
+HB_FUNC(GETNOTIFYCODE)
 {
 }
 
-HB_FUNC( LISTVIEWNOTIFY )
+HB_FUNC(TREENOTIFY)
 {
 }
 
+HB_FUNC(LISTVIEWNOTIFY)
+{
+}
